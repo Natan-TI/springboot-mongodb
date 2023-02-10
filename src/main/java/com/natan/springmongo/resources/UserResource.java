0b4a623ad77ecba006f78b054168c88388b27849
Natan-TI <natan201125@hotmail.com>
2023-02-10 02:53:31 -0300

    Endpoint to return a User's posts

--- src/main/java/com/natan/springmongo/resources/UserResource.java
+++ src/main/java/com/natan/springmongo/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.natan.springmongo.domain.Post;
 import com.natan.springmongo.domain.User;
 import com.natan.springmongo.dto.UserDTO;
 import com.natan.springmongo.services.UserService;
@@ -63,5 +64,11 @@ public class UserResource {
 		user.setId(id); 
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
